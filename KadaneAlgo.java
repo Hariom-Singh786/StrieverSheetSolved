41ee8ed9d095e4479ebb9cced21bb798d6a87045
Hariom <91545175+Hariom-Singh786@users.noreply.github.com>
2022-05-04 01:19:08 +0530

    Update KadaneAlgo.java

--- KadaneAlgo.java
+++ KadaneAlgo.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        // optimized solution
         int sum=0;
         int maxsum=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++)
@@ -13,6 +14,24 @@ class Solution {
         }
         return maxsum;
       
+        
+         
+        // TLE solution
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            int currSum=0;
+            for(int j=i;j<nums.length;j++)
+            {
+                currSum+=nums[j];
+                if(currSum>maxi)
+                {
+                    maxi = currSum;
+                }
+                
+            }
+        }
+        return maxi;
     }
 }
         
