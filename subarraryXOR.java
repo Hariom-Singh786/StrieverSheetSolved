cf011725883c2b3f08fbab84236e47ecdb186182
Hariom <91545175+Hariom-Singh786@users.noreply.github.com>
2022-06-15 08:44:40 +0530

    Update subarraryXOR.java

--- subarraryXOR.java
+++ subarraryXOR.java
@@ -1,6 +1,28 @@
 
 public class Solution {
 	public static int subarraysXor(ArrayList<Integer> arr, int x) {
+       
+	//optimized approach
+        HashMap<Integer,Integer>freq = new HashMap<>();
+        int xor=0,count=0,n = arr.size();
+        for(int i=0;i<n;i++)
+        {
+            xor = xor ^ arr.get(i);
+            if(freq.get(xor ^ x)!=null)
+            {
+                count +=freq.get(xor ^ x);
+            }
+             if(xor == x) count++;
+             if(freq.get(xor)!=null)
+              {
+                 freq.put(xor, freq.get(xor) + 1);
+              }
+              else  freq.put(xor,1);
+        }
+        return count;
+		
+		
+        // naive approach
         int count=0;
         int n = arr.size();
         for(int i=0;i<n;i++)
