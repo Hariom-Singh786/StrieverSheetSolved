66b77dc6b18704940db409a1a785f5d6c328c34f
Hariom <91545175+Hariom-Singh786@users.noreply.github.com>
2022-06-24 00:58:29 +0530

    Create validParenthesis.java

---
+++ validParenthesis.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char it : s.toCharArray())
+        {
+            if(it=='(' ||it=='{' || it =='[')
+            {
+                st.push(it);
+            }
+            else
+            {
+                if(st.isEmpty())return false;
+                char ch = st.pop();
+                if(ch=='(' && it==')' ||ch=='{' && it=='}'|| ch =='[' && it==']')
+                    continue;
+                else
+                    return false;
+            }
+        }
+       return st.isEmpty();
+    }
+}
